a11e9bb773ce2e05c457972360f8cebffab36bfd
Abhishek <abhishek.ranjan@quantiphi.com>
2024-03-28 17:22:59 +0530

    updated lambda fn 2 code

--- src/lambda/lambda_function_2/lambda_function_2.js
+++ src/lambda/lambda_function_2/lambda_function_2.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from lambda_function_2_v3'),
+        body: JSON.stringify('Hello from lambda_function_2_v4'),
     }
     return response 
 }
