ff099c790e265484090cdd8f7a33b93255886e24
Abhishek <abhishek.ranjan@quantiphi.com>
2024-04-02 14:15:20 +0530

    updated main.yml

--- backend.tf
+++
@@ -1,8 +0,0 @@
-# Define Terraform backend using a S3 bucket for storing the Terraform state
-terraform {
-  backend "s3" {
-    bucket = "terraform-statefiles-bucket-163450518166"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- src/lambda/lambda_function_5/lambda_function_5.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from lambda_function_5'),
-    }
-    return response 
-}
