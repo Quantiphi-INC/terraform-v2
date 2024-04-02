1120468e68f39b01fd859d0af152e8cc951c215c
Abhishek <abhishek.ranjan@quantiphi.com>
2024-04-02 19:20:40 +0530

    removed new Glue job

--- terraform/glue.tf
+++ terraform/glue.tf
@@ -32,43 +32,6 @@ resource "aws_glue_job" "test_deploy_script" {
   }
 }
 
-
-resource "aws_s3_object" "create_script_s3" {
-  bucket = var.s3_bucket
-  key    = "glue/scripts/NewTestDeployScript.py"
-  source = "${local.glue_src_path_2}NewTestDeployScript.py"
-  etag   = filemd5("${local.glue_src_path_2}NewTestDeployScript.py")
-}
-
-resource "aws_glue_job" "new_test_deploy_script" {
-  glue_version      = "4.0"                                                                       #optional
-  max_retries       = 0                                                                           #optional
-  name              = var.glue_job_name_1                                                         #required
-  description       = "test the deployment of an aws glue job to aws glue service with terraform" #description
-  role_arn          = module.glue_service_role.iam_role_arn                                       #required
-  number_of_workers = 2                                                                           #optional, defaults to 5 if not set
-  worker_type       = "G.1X"                                                                      #optional
-  timeout           = "60"                                                                        #optional
-  execution_class   = "FLEX"                                                                      #optional
-  command {
-    name            = "glueetl"                                                 #optional
-    script_location = "s3://${var.s3_bucket}/${var.glue_job_test_script_loc_1}" #required
-  }
-  default_arguments = {
-    "--class"                   = "GlueApp"
-    "--enable-job-insights"     = "true"
-    "--enable-auto-scaling"     = "false"
-    "--enable-glue-datacatalog" = "true"
-    "--job-language"            = "python"
-    "--job-bookmark-option"     = "job-bookmark-disable"
-    "--datalake-formats"        = "iceberg"
-    "--conf"                    = "spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions  --conf spark.sql.catalog.glue_catalog=org.apache.iceberg.spark.SparkCatalog  --conf spark.sql.catalog.glue_catalog.warehouse=s3://tnt-erp-sql/ --conf spark.sql.catalog.glue_catalog.catalog-impl=org.apache.iceberg.aws.glue.GlueCatalog  --conf spark.sql.catalog.glue_catalog.io-impl=org.apache.iceberg.aws.s3.S3FileIO"
-
-  }
-}
-
-
-
 module "glue_service_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
   version = "5.5.2"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,11 +2,6 @@ locals {
   glue_src_path = "../src/glue/test_glue_job/"
 }
 
-locals {
-  glue_src_path_2 = "../src/glue/new_test_glue_job/"
-}
-
-
 variable "s3_bucket" {
   type = string
 }
@@ -24,11 +19,5 @@ variable "glue_job_name" {
   type = string
 }
 variable "glue_job_test_script_loc" {
-  type = string
-}
-variable "glue_job_name_1" {
-  type = string
-}
-variable "glue_job_test_script_loc_1" {
   type = string
 }
